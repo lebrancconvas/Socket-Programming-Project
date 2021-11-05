944af41ff753e870aaeffb5c81f60ffa5597b370
lebrancconvas <lebrancconvas@gmail.com>
2021-11-05 07:10:58 +0700

    🔨  Change Servername.

--- example/tcp/client.py
+++ example/tcp/client.py
@@ -1,6 +1,6 @@
 from socket import *
 
-serverName = 'servername'
+serverName = 'localhost' 
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
