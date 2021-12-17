f8f22312277a41009c14030543ac1dd267bed31f
lebrancconvas <lebrancconvas@gmail.com>
2021-12-17 20:30:10 +0700

    🔨 Update UDP.

---
+++ src/udp/client.py
@@ -0,0 +1,17 @@
+import socket 
+
+port = 12000
+server = "192.168.1.132"
+
+# Create Socket Object. 
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+message = input("Input: ")
+
+clientSocket.sendto(message.encode(),(server, port))
+
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+print(f"[(serverAddress)] {modifiedMessage.decode()}")
+
+clientSocket.close() 
